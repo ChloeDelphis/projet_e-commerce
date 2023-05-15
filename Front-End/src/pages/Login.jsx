6cd96636e20487c7c1d43fcbd9ca61463e04a7e0
Laura Pereira <laurap4998@gmail.com>
2023-05-15 16:08:16 +0200

    pour push sur dev

--- Front-End/src/pages/Login.jsx
+++ Front-End/src/pages/Login.jsx
@@ -4,11 +4,11 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import { IonIcon } from '@ionic/react';
 import { mailOutline, lockClosedOutline, personOutline, locationOutline, callOutline } from 'ionicons/icons';
-
+import SignIn from '../components/SignIn';
+import { Value } from 'sass';
 
 // La page de Login (connexion)
 const Login = () => {
-
 
   return (
     <>
@@ -26,13 +26,13 @@ const Login = () => {
                 <label for="">Email</label>
               </div>
 
-              <div class="inputbox">
+              <div className="inputbox">
                 <IonIcon icon={lockClosedOutline} />
                 <input type="password" required></input>
                 <label for="">Password</label>
               </div>
 
-              <div class="ligne">
+              <div className="ligne">
                 <label for=""><input type="checkbox"></input>Remember Me </label>
                 <a href="">Forget Password</a>
               </div>
@@ -44,42 +44,42 @@ const Login = () => {
 
         <div className="signup">
           <div className="form-box">
-              <form>
+              <form onSubmit={SignIn}>
 
                 <h2>Sign in</h2>
 
                 <div className="inputbox">
                   <IonIcon icon={mailOutline} />
-                  <input type="email" required></input>
+                  <input type="email" required value={SignIn.arguments.email} onChange={(e) => SignIn.setEmail(e.target.value)}></input>
                   <label for="">Email</label>
                 </div>
 
-                <div class="inputbox">
+                <div className="inputbox">
                   <IonIcon icon={lockClosedOutline} />
                   <input type="password" required></input>
                   <label for="">Password</label>
                 </div>
 
-                <div class="ligne">
-                  <div class="inputbox">
+                <div className="ligne">
+                  <div className="inputbox">
                   <IonIcon icon={personOutline} />
                       <input type="text" required></input>
                       <label for="">Nom</label>
                   </div>
-                  <div class="inputbox">
+                  <div className="inputbox">
                       <IonIcon icon={personOutline} />
                       <input type="text" required></input>
                       <label for="">Prenom</label>
                   </div>
                 </div>
 
-                <div class="inputbox">
+                <div className="inputbox">
                     <IonIcon icon={locationOutline} />
                     <input type="text" required></input>
                     <label for="">Adresse</label>
                 </div>
 
-                <div class="inputbox">
+                <div className="inputbox">
                 <IonIcon icon={callOutline} />
                     <input type="text" required></input>
                     <label for="">Telephone</label>
