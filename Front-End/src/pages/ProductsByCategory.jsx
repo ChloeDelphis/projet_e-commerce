b203bb38d80993c44232a8c48a664661805fff12
Chloe Delphis <chloe.delphis@gmail.com>
2023-05-16 19:27:44 +0200

    dynamic display of product list of one category

--- Front-End/src/pages/ProductsByCategory.jsx
+++ Front-End/src/pages/ProductsByCategory.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 // Page qui affiche tous les produits en fonction d'une catégorie
@@ -7,11 +8,21 @@ const ProductsByCategory = () => {
   // id de la catégorie à afficher
   const { id } = useParams();
 
+  const [categorie, setCategorie] = useState();
+
+  useEffect(() => {
+    console.log(id);
+
+    fetch(`http://localhost:8080/site/categories/${id}`)
+      .then((res) => res.json())
+      .then((data) => setCategorie(data));
+  }, []);
+
   return (
     <div className="productsbycategory">
-      <CategoryHeader data={dataOne} />
-      <OrderandFilter data={dataOne} />
-      <CategoryDetail data={dataOne} />
+      <CategoryHeader data={categorie} />
+      <OrderandFilter />
+      <CategoryDetail data={categorie} />
     </div>
   );
 };
@@ -19,13 +30,13 @@ const ProductsByCategory = () => {
 const CategoryHeader = ({ data }) => {
   return (
     <div className="header">
-      <h1 className="header__title">{data.categorie.name}</h1>
-      <h2 className="header__description">{data.categorie.description}</h2>
+      <h1 className="header__title">{data && data.name}</h1>
+      <h2 className="header__description">{data && data.name}</h2>
     </div>
   );
 };
 
-const OrderandFilter = ({ data }) => {
+const OrderandFilter = () => {
   return (
     <form className="orderandfilter">
       <div className="orderandfilter__order">
@@ -42,17 +53,19 @@ const OrderandFilter = ({ data }) => {
         </select>
       </div>
       <div className="orderandfilter__filter">
-        <label className="orderandfiler__filter__label">Filtrer par :</label>
+        <label className="orderandfiler__filter__label">
+          Filtrer par marque :
+        </label>
         <select
           name="orderby"
           id="orderby"
           className="shopping__buy__filter__choices"
         >
-          {data.categorie.articles.map((item, index) => (
+          {/* {data.categorie.articles.map((item, index) => (
             <option key={index} value="priceasc">
               {item.marque}
             </option>
-          ))}
+          ))} */}
         </select>
       </div>
     </form>
@@ -63,94 +76,21 @@ const OrderandFilter = ({ data }) => {
 const CategoryDetail = ({ data }) => {
   return (
     <div className="list">
-      {data.categorie.articles.map((item, index) => (
-        <div className="list__item" key={index}>
-          <img className="list__item__img" src={item.img} alt="product_image" />
-          <h3 className="list__item__name">{item.nom}</h3>
-          <h4 className="list__item__brand"> {item.marque}</h4>
-          <p className="list__item__price">{item.prix},00 €</p>
-        </div>
-      ))}
+      {data &&
+        data.articles.map((item, index) => (
+          <div className="list__item" key={index}>
+            <img
+              className="list__item__img"
+              src={item.img}
+              alt="product_image"
+            />
+            <h3 className="list__item__name">{item.nom}</h3>
+            <h4 className="list__item__brand"> {item.marque}</h4>
+            <p className="list__item__price">{item.prix},00 €</p>
+          </div>
+        ))}
     </div>
   );
-};
-
-const dataOne = {
-  categorie: {
-    id: 1,
-    name: "T-shirts",
-    description: "Des t-shirts à la coule",
-    image: null,
-    mea: 0,
-    articles: [
-      {
-        ref: 101,
-        nom: "Led Zeppelin",
-        marque: "Music",
-        description:
-          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam veniam maxime minus iure, amet placeat quo quidem ducimus quam cumque hic sapiente? Id, eius quia.",
-        prix: 35.0,
-        img: "../assets/products/1_t-shirts/1.jpg",
-        mea: 0,
-        date: "2022-03-02T00:00:00.000+0000",
-      },
-      {
-        ref: 102,
-        nom: "Country Roads",
-        marque: "Music",
-        description:
-          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam veniam maxime minus iure, amet placeat quo quidem ducimus quam cumque hic sapiente? Id, eius quia.",
-        prix: 30.0,
-        img: "../assets/products/1_t-shirts/2.jpg",
-        mea: 0,
-        date: "2022-03-03T00:00:00.000+0000",
-      },
-      {
-        ref: 103,
-        nom: "Cactus & Bloom",
-        marque: "In Bloom",
-        description:
-          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam veniam maxime minus iure, amet placeat quo quidem ducimus quam cumque hic sapiente? Id, eius quia.",
-        prix: 25.0,
-        img: "../assets/products/1_t-shirts/3.jpg",
-        mea: 0,
-        date: "2022-03-04T00:00:00.000+0000",
-      },
-      {
-        ref: 104,
-        nom: "Flower Charts",
-        marque: "In Bloom",
-        description:
-          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam veniam maxime minus iure, amet placeat quo quidem ducimus quam cumque hic sapiente? Id, eius quia.",
-        prix: 20.0,
-        img: "../assets/products/1_t-shirts/4.jpg",
-        mea: 0,
-        date: "2022-04-01T00:00:00.000+0000",
-      },
-      {
-        ref: 105,
-        nom: "Marguerite",
-        marque: "In Bloom",
-        description:
-          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam veniam maxime minus iure, amet placeat quo quidem ducimus quam cumque hic sapiente? Id, eius quia.",
-        prix: 24.0,
-        img: "../assets/products/1_t-shirts/5.jpg",
-        mea: 0,
-        date: "2022-05-01T00:00:00.000+0000",
-      },
-      {
-        ref: 106,
-        nom: "Since 1971",
-        marque: "Vintage",
-        description:
-          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam veniam maxime minus iure, amet placeat quo quidem ducimus quam cumque hic sapiente? Id, eius quia.",
-        prix: 22.0,
-        img: "../assets/products/1_t-shirts/6.jpg",
-        mea: 0,
-        date: "2022-06-01T00:00:00.000+0000",
-      },
-    ],
-  },
 };
 
 export default ProductsByCategory;
