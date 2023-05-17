a8412cb7a92418ab3a8385f4df8a8d159c416f41
Chloe Delphis <chloe.delphis@gmail.com>
2023-05-17 23:04:27 +0200

    debut

--- Front-End/src/components/HomeTop.jsx
+++ Front-End/src/components/HomeTop.jsx
@@ -5,22 +5,14 @@ const HomeTop = () => {
     <div className="homepage_top">
       {/* partie gauche */}
       <div className="top_left">
-        <div className="ellipse"></div>
-        <h1>Medium length hero headline goes here</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          varius enim in eros elementum tristique. Duis cursus, mi quis viverra
-          ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.
-        </p>
-        <button>Start Looking</button>
+        <h1>Les Robes sont à l'honneur !</h1>
+        <p>Oh les belles robes !</p>
+        <button>Tout voir</button>
       </div>
 
       {/* partie droite */}
       <div className="top_right">
-        <img
-          src="./assets/components/carousel/carousel1.png"
-          alt="image_carousel"
-        />
+        <img src="./assets/products/5_robes/1.jpg" alt="image_carousel" />
       </div>
     </div>
   );
