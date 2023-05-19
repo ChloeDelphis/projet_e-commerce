1cb84b1100f169573dfd6f19eaf99818cadbfe06
Ionathan Gusmini <ionathangusmini@gmail.com>
2023-05-19 09:01:01 +0000

    update about+contact

--- Front-End/src/pages/About.jsx
+++ Front-End/src/pages/About.jsx
@@ -2,7 +2,36 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div>About US</div>
+        <div className='about'>
+            <h2>À propos de ce site</h2>
+            <div className="main">
+                Pendant le développement de notre site de vente de vêtements en ligne,
+                nous avons travaillé en équipe en utilisant des technologies modernes pour
+                créer une application robuste et fonctionnelle. Nous avons utilisé React pour
+                développer l'interface utilisateur interactive, Java et Java Spring pour gérer
+                la logique métier côté serveur, et MySQL pour stocker et récupérer les données.
+                <br /><br />
+                En plus de ces technologies, nous avons également exploré d'autres frameworks
+                et bibliothèques tels que JPA (Java Persistence API) avec le modèle MVC
+                (Model-View-Controller) pour gérer la persistance des données et le routage des requêtes.
+                <br /><br />
+                Nous avons également utilisé Git pour la gestion du contrôle de version,
+                ce qui nous a permis de travailler efficacement en équipe, de suivre les modifications apportées au code et de résoudre les conflits éventuels.
+                <br /><br />
+                Ce projet nous a donné l'opportunité de mettre en pratique tout ce que
+                nous avons appris jusqu'à présent dans notre formation de développeur.
+                Il nous a permis de combiner nos connaissances en développement front-end
+                avec les compétences en développement back-end, et d'intégrer plusieurs
+                technologies pour créer une application complète et fonctionnelle.
+                <br /><br />
+                Enfin, nous aimerions exprimer notre gratitude envers nos formateurs
+                qui nous ont accompagnés tout au long de ce projet. Leurs conseils,
+                leur expertise et leur soutien ont été inestimables pour notre
+                apprentissage et notre réussite. Nous sommes reconnaissants de cette
+                expérience enrichissante et nous sommes impatients de continuer à
+                développer nos compétences dans le domaine du développement.
+            </div>
+        </div>
     )
 }
 
